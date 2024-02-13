ca80666ec62f61ae421c5fb943b8105629f501ab
Abhay Raj Singh <abhayraja4@gmail.com>
2024-02-13 15:30:47 -0500

    correct todos html redirection

--- todos.py
+++ todos.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, url_for, redirect, Blueprint
+from flask import render_template, request, url_for, redirect, Blueprint
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
 
 bp = Blueprint('todos', __name__)
 
@@ -13,36 +12,36 @@ todos = db.todos
 
 def parse_todo_doc(__doc):
     return {
-            'id':__doc.get('id', 1),
-            #'_id':__doc.get('_id', 'UUID'),
-            'task':__doc.get('task', 'Task'),
-            'isCompleted':__doc.get('isCompleted', True)
-            }
+        'id': __doc.get('id', 1),
+        'task': __doc.get('task', 'Task'),
+        'isCompleted': __doc.get('isCompleted', True)
+    }
 
 
 @bp.route('/ui', methods=('GET', 'POST'))
 def index():
-    if request.method=='POST':
+
+    if request.method == 'POST':
         content = request.form['content']
         degree = request.form['degree']
         is_todo_completed = degree == 'Important'
         new_todo = {
-                'content': content, 
-                'degree': degree,
-                'task': content,
-                'isCompleted': is_todo_completed 
-                }
+            'content': content,
+            'degree': degree,
+            'task': content,
+            'isCompleted': is_todo_completed
+        }
         todos.insert_one(new_todo)
-        return redirect(url_for('index'))
+        return redirect(url_for('todos.index'))
 
     all_todos = todos.find()
     return render_template('index.html', todos=all_todos)
 
 
-@bp.post('/<id>/delete/')
-def delete(id):
-    todos.delete_one({"_id": ObjectId(id)})
-    return redirect(url_for('index'))
+@bp.post('/<oid>/delete/')
+def delete(oid):
+    todos.delete_one({"_id": ObjectId(oid)})
+    return redirect(url_for('todos.index'))
 
 
 @bp.route('/')
