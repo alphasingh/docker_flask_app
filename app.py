9e52725331e4e07a8eea414c5db853ff69a6e70c
Abhay Raj Singh <abhayraja4@gmail.com>
2024-09-27 14:24:31 -0400

    update HTML for mobiles

--- app.py
+++ app.py
@@ -6,15 +6,14 @@ import others
 import rate_limits
 import todos
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 PATH = '/flaskapi/v1'
 
-app.register_blueprint(home.home_bp, url_prefix=PATH+'/home')
-app.register_blueprint(contact.contact_bp, url_prefix=PATH+'/contact')
-app.register_blueprint(rate_limits.rate_limits_bp, url_prefix=PATH+'/ratelimits')
-app.register_blueprint(others.others_bp, url_prefix=PATH+'/others')
-app.register_blueprint(todos.bp, url_prefix=PATH+'/todos')
-
+app.register_blueprint(home.home_bp, url_prefix=PATH + '/home')
+app.register_blueprint(contact.contact_bp, url_prefix=PATH + '/contact')
+app.register_blueprint(rate_limits.rate_limits_bp, url_prefix=PATH + '/ratelimits')
+app.register_blueprint(others.others_bp, url_prefix=PATH + '/others')
+app.register_blueprint(todos.bp, url_prefix=PATH + '/todos')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("5909"))
